ab2634e684ed5ffb9146a812a7639ef8beda251d
Marie Curie Salera <mcsalera@gmail.com>
2018-01-30 14:28:50 +0800

    added punctuation feature

--- Excercise1_RegEx/RegEx.pl
+++ Excercise1_RegEx/RegEx.pl
@@ -23,4 +23,27 @@ for(@array_of_words){
 my $items = scalar @array_of_words;
 say "Number of tokens: ", $items;
 
- close($file_handler)	
+close($file_handler);	
+
+my @puncList = ('\.', ',', ':', ';', '!', '\?', '\'', '"', '-', '\[', '\]', '\(', '\)', '\{', '\}');
+my @printPuncList = (".", ",", ":", ";", "!", "?", '"', "'", "-", "[", "]", "(", ")", "{", "}");
+my @arrayPuncCount = ();
+
+my $arrayString = (".safa.potato,hehehehe , someomfa:"); 
+my $size = scalar @puncList;
+
+my $find;
+my $count = 0;
+my $maxIndex = -1;
+for(my $i = 0 ; $i < $size ; $i = $i + 1){
+   $find = $puncList[$i];
+   $count = () = $arrayString =~ m(($find))g;
+   $arrayPuncCount[$i] = $count;
+
+   $count = 0;
+}
+for (my $i = 0; $i < $size; $i++) {
+	if($arrayPuncCount[$i] > 0){
+		say $printPuncList[$i], " ", $arrayPuncCount[$i];
+	}
+}
