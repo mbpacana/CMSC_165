347b171f667bb201719b9b3a7ca73e0fedfe5cf1
Marie Curie Salera <mcsalera@gmail.com>
2018-01-30 16:03:42 +0800

    updated counter

--- Excercise1_RegEx/RegEx.pl
+++ Excercise1_RegEx/RegEx.pl
@@ -6,9 +6,9 @@ use feature "switch";
 use v5.16;
 
 my $file_path = "test.txt";
-my @puncList = ('\.', ',', ':', ';', '!', '\?', '\'', '"', '-', '\[', '\]', '\(', '\)', '\{', '\}');
-my @printPuncList = (".", ",", ":", ";", "!", "?", "'", "'", "-", "[", "]", "(", ")", "{", "}");
-my @arrayPuncCount = ();
+my @puncList = ('\.', ',', ':', ';', '!', '\?', '\'', '\"', '-', '\[', '\]', '\(', '\)', '\{', '\}');
+my @printPuncList = (".", ",", ":", ";", "!", "?", "'", '"', "-", "[", "]", "(", ")", "{", "}");
+my @arrayPuncCount = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
 my $size = scalar @puncList;
 open my $file_handler, '<' , $file_path or die "Unable to open $file_path for reading: $!";
@@ -31,25 +31,7 @@ say "Number of tokens: ", $items;
 
 close($file_handler);	
 
-<<<<<<< HEAD
-my @puncList = ('\.', ',', ':', ';', '!', '\?', '\'', '"', '-', '\[', '\]', '\(', '\)', '\{', '\}');
-my @printPuncList = (".", ",", ":", ";", "!", "?", '"', "'", "-", "[", "]", "(", ")", "{", "}");
-my @arrayPuncCount = ();
 
-my $arrayString = (".safa.potato,hehehehe , someomfa:"); 
-my $size = scalar @puncList;
-
-my $find;
-my $count = 0;
-my $maxIndex = -1;
-for(my $i = 0 ; $i < $size ; $i = $i + 1){
-   $find = $puncList[$i];
-   $count = () = $arrayString =~ m(($find))g;
-   $arrayPuncCount[$i] = $count;
-
-   $count = 0;
-}
-=======
 sub count_punctuation {
 	my ($string, $size) = @_;
 	my $find;
@@ -58,13 +40,11 @@ sub count_punctuation {
 	for(my $i = 0 ; $i < $size ; $i = $i + 1){
 	   $find = $puncList[$i];
 	   $count = () = $string =~ m(($find))g;
-	   $arrayPuncCount[$i] = $count;
+	   $arrayPuncCount[$i] += $count;
 	   $count = 0;
 	}
 }
 
-
->>>>>>> cf2f9e9a69187ef2313e0ffe29368cdb8d53a4b0
 for (my $i = 0; $i < $size; $i++) {
 	if($arrayPuncCount[$i] > 0){
 		say $printPuncList[$i], " ", $arrayPuncCount[$i];
